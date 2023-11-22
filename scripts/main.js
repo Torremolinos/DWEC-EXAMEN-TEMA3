35c3af4128cca7f9ba8e8226fa4d48774f16129a
Torremolinos <adrian_2054@hotmail.com>
2023-11-22 08:49:02 +0100

    Hago el primer cambio, que es cambiar innerHeight primero por innerWidth

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerHeight;
+//Aqui hago un cambio en el innerWidth porque los dos eran height y uno debe ocuparse del largo y otro del ancho.
+const width = canvas.width = window.innerWidth;  
 const height = canvas.height = window.innerHeight;
 
 function random(min, max) {
